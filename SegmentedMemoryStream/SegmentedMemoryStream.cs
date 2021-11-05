94c26ab6224f83cd246e022f4396841c522b8ddd
BazookaMusic <sotirisdragonas@gmail.com>
2021-11-05 03:36:41 +0100

    remove unused dependency

--- SegmentedMemoryStream/SegmentedMemoryStream.cs
+++ SegmentedMemoryStream/SegmentedMemoryStream.cs
@@ -28,7 +28,6 @@ namespace Bazooka.SegmentedMemoryStream
 
         private readonly List<byte[]> segments;
 
-        private bool disposed;
 
         private int CurrentSegmentIndex => (int)((ulong)this.Position >> this.bitShiftForModulo);
 
